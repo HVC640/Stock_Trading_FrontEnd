6efc7d4b4c7cd21f4202b67f12a27a162b1e628a
Maheshshinde1191 <maheshshinde1191@gmail.com>
2023-03-28 10:53:53 +0530

    components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageModule } from './homepage/homepage.module';
+import { TradeModule } from './trade/trade.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./homepage/homepage.component').then(
+            (m) => HomepageModule,
+          ),
+      },
+      {
+        path:'trade',
+        loadChildren:()=> 
+          import('./trade/trade.component').then(
+              (m) => TradeModule,
+        ),
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { TradeComponent } from './trade/trade.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    TradeComponent,
+    
   ],
   imports: [
     BrowserModule,
