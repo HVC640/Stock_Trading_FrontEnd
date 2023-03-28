59e1c0b8951b71a9312758f7eb373845b0e0b788
Harsh Chaudhari <harshc640@gmail.com>
2023-03-28 11:18:50 +0530

    Components Made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { OrdersComponent } from './Pages/orders/orders.component';
+import { HoldingsComponent } from './Pages/holdings/holdings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    OrdersComponent,
+    HoldingsComponent
   ],
   imports: [
     BrowserModule,
