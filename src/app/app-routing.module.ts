62e8755f16421462bdea1ae87fdea8d5ead3589d
Maheshshinde1191 <maheshshinde1191@gmail.com>
2023-03-28 11:42:30 +0530

    some change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomepageModule } from './homepage/homepage.module';
-import { TradeModule } from './trade/trade.module';
 
 const routes: Routes = [
   {
-    path: "",
-    children: [
-      {
-        path: '',
-        loadChildren: () =>
-          import('./homepage/homepage.component').then(
-            (m) => HomepageModule,
-          ),
-      },
-      {
-        path:'trade',
-        loadChildren:()=> 
-          import('./trade/trade.component').then(
-              (m) => TradeModule,
-        ),
-      }
-    ]
+    
   }
 ];
 
