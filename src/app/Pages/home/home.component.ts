f7f264461bf6b4f541ef42e11c8f764791e83f19
Harsh Chaudhari <harshc640@gmail.com>
2023-03-30 16:59:05 +0530

    First Commit

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -12,4 +12,11 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  arrCompany = [
+    { name: "TANLA", change: -29.10, changePercent: "-5.40%", value: 509.80 },
+    { name: "ATUL", change: 53.05, changePercent: "0.77%", value: 6974.60 },
+    { name: "EPL", change: 6.80, changePercent: "4.35%", value: 163.10 },
+    { name: "IRCON", change: -0.25, changePercent: "-0.47%", value: 52.90 }
+  ]
+
 }
